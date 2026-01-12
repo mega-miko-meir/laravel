f189ffd68d87f5c67ad9d29f6d915bca83ad32b9
Meirzhan Akimbekov <meir.akimbek@gmail.com>
2026-01-12 09:47:42 +0500

    rolling back two role migrations

--- database/migrations/2026_01_09_144421_add_role_id_to_users_table.php
+++ database/migrations/2026_01_09_144421_add_role_id_to_users_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')->default(1);
-            $table->foreign('role_id')->references('id')->on('roles');
+            //
         });
     }
 
@@ -23,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign(['role_id']);
-            $table->dropColumn('role_id');
+            //
         });
     }
 };
